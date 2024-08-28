fa6bfc6c63eb98730e54dd03d27d3d1dfd7dd015
Jhon Cruz <j@loscruz.co>
2024-08-27 20:43:35 -0500

    memory

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 //Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
 import { OpenAI } from "@langchain/openai";
+import { BufferMemory } from "langchain/memory";
+import { ConversationChain } from "langchain/chains";
 
 //Import the PromptTemplate module
 import { PromptTemplate } from "@langchain/core/prompts";
@@ -9,22 +11,43 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const run = async () => {
+  //Instantiate the BufferMemory passing the memory key for storing state
+  const memory = new BufferMemory({ memoryKey: "chat_history" });
+
   //Instantiante the OpenAI model
   //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
   const model = new OpenAI({ temperature: 0.9 });
 
   //Create the template. The template is actually a "parameterized prompt". A "parameterized prompt" is a prompt in which the input parameter names are used and the parameter values are supplied from external input
-  const template = "What is a good name for a company that makes {product}?";
+  //Note the input variables {chat_history} and {input}
+  const template = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+      Current conversation:
+      {chat_history}
+      Human: {input}
+      AI:`;
 
-  //Instantiate "PromptTemplate" passing the prompt template string initialized above and a list of variable names the final prompt template will expect
-  const prompt = new PromptTemplate({ template, inputVariables: ["product"] });
+  //Instantiate "PromptTemplate" passing the prompt template string initialized above
+  const prompt = PromptTemplate.fromTemplate(template);
 
-  //Instantiate LLMChain, which consists of a PromptTemplate and an LLM. Pass the result from the PromptTemplate and the OpenAI LLM model
-  const chain = prompt.pipe(model);
+  const chain = new ConversationChain({
+    memory: memory,
+    prompt: prompt,
+    llm: model,
+  });
 
-  //Run the chain. Pass the value for the variable name that was sent in the "inputVariables" list passed to "PromptTemplate" initialization call
-  const res = await chain.invoke({ product: "colorful socks" });
-  console.log({ res });
+  //Run the chain passing a value for the {input} variable. The result will be stored in {chat_history}
+  const res1 = await chain.call({ input: "Hi! I'm Morpheus." });
+  console.log({ res1 });
+
+  //Run the chain again passing a value for the {input} variable. This time, the response from the last run ie. the  value in {chat_history} will alo be passed as part of the prompt
+  const res2 = await chain.call({ input: "What's my name?" });
+  console.log({ res2 });
+
+  //BONUS!!
+  const res3 = await chain.call({
+    input: "Which epic movie was I in and who was my protege?",
+  });
+  console.log({ res3 });
 };
 
 run();
